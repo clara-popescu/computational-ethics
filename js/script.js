8ed4051520122355b2653d43dd3237b78016e9aa
clara <c.popescu0520231@arts.ac.uk>
2025-12-05 10:46:39 +0000

    text animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// map
-gsap.from(".draw-me", {duration:4, drawSVG: 0});
+
+
+// text pinning
+// pin the section behind the text until text-5 is in its final position
+ScrollTrigger.create({
+  id: "sectionPin",
+  trigger: "#section",
+  start: "top top",
+  end: () => "+=" + (window.innerHeight * 5),
+  pin: true,
+  pinSpacing: true
+});
+
+gsap.utils.toArray(["#text-2", "#text-3", "#text-4", "#text-5"]).forEach((el, i) => {
+  gsap.fromTo(el,
+    { y: 1000},
+    {
+    y: 0,     
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#section",
+      start: () => ScrollTrigger.getById("sectionPin").start + window.innerHeight * i,
+      end: () => ScrollTrigger.getById("sectionPin").start + window.innerHeight * (i + 1),
+      scrub: true
+    }
+  }
+  );
+});
 
 // balance
 gsap.to("#bar", {
