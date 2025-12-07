3774c6ef2b41e6db611266ed1032e22935b754ad
clara <c.popescu0520231@arts.ac.uk>
2025-12-07 17:44:38 +0000

    second page basic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
-
+gsap.registerPlugin(DrawSVGPlugin);
 
 
 // text pinning
@@ -17,7 +17,7 @@ gsap.utils.toArray(["#text-2", "#text-3", "#text-4", "#text-5"]).forEach((el, i)
   gsap.fromTo(el,
     { y: 1000},
     {
-    y: 0,     
+    y: 0,
     ease: "none",
     scrollTrigger: {
       trigger: "#section",
@@ -27,36 +27,4 @@ gsap.utils.toArray(["#text-2", "#text-3", "#text-4", "#text-5"]).forEach((el, i)
     }
   }
   );
-});
-
-// balance
-gsap.to("#bar", {
-  rotate: -25,
-  transformOrigin: "50%, 50%",
-  scrollTrigger: { 
-    trigger: "#title", 
-    start: "top 80%",
-    // markers: true,
-    scrub: true }
-});
-
-gsap.to("#leftPlate", {
-  y: 90,
-  transformOrigin: "50%, 50%",
-  scrollTrigger: { 
-    trigger: "#title", 
-    start: "top 80%",
-    // markers: true,
-    scrub: true }
-});
-
-gsap.to("#rightPlate", {
-  y: -90,
-  transformOrigin: "50%, 50%",
-  scrollTrigger: { 
-    trigger: "#title", 
-    // start: top (top position of the target - 0px from the target element), 80% (scroller start is 80% down the viewport)
-    start: "top 80%",
-    markers: true,
-    scrub: true }
 });
